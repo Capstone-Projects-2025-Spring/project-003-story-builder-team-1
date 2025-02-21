e83b314af9c149ee8ca82e0341d1890127bc047b
Kamaljeeth Vijay <kjeeth04@gmail.com>
2025-02-21 00:59:48 -0500

    Added Comments

--- storybuilder/Backend/Backend.js
+++ storybuilder/Backend/Backend.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//CORS Policy Handling
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
+//Status Message to Home Page of Server
 app.get('/', (req, res) => {
     res.send("Backend Server Active");
 })
