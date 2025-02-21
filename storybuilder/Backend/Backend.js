6f695b33719e4e97b3f30d292a5ba7f3d57c7e19
Kamaljeeth Vijay <kjeeth04@gmail.com>
2025-02-21 00:56:09 -0500

    Update Backend.js
    Added CORS policy, and gave basic status .get to the '/' page

--- storybuilder/Backend/Backend.js
+++ storybuilder/Backend/Backend.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/', (req, res) => {
+    res.send("Backend Server Active");
+})
+
 //Receiving and (For Now) Displaying Input from Text Box in Console
 app.post('/api/text_box/', (req, res) => {
     const input = JSON.stringify(req.body);
