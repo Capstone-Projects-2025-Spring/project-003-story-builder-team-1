eb92ceac11366e2623d7612b5a09ac79234e0a7b
Kamaljeeth Vijay <kjeeth04@gmail.com>
2025-03-17 19:39:43 -0400

    More tests added

--- storybuilder/Backend/__test__/translator.test.js
+++ storybuilder/Backend/__test__/translator.test.js
@@ -25,10 +25,10 @@ describe('POST /app/chapter_count/', () => {
 //Test Case 2: Chapter Count Unsuccessfully Receive Non-Numeric Data
 describe('POST /app/chapter_count/', () => {
 
-  it('should return 400 and a error message with the input data', async () => {
+  it('should return 400 and an error message with the input data', async () => {
     
     //Define input data
-    const inputData = {"data": "fail"};
+    const inputData = {"data": "Test"};
     
     //Post Request
     const response = await request(app)
@@ -39,6 +39,132 @@ describe('POST /app/chapter_count/', () => {
 
     //assert response matches expected output
     expect(response.body).toEqual({message: "Invalid Data Type", data: inputData});
+
+  });
+});
+
+//Test Case 3: Story Details Successfully Receives Story Details
+describe('POST /app/story_details/', () => {
+
+  it('should return 200 and a success message with the input data', async () => {
+    
+    //Define input data
+    const inputData = {"data": "Story Detail"};
+    
+    //Post Request
+    const response = await request(app)
+      .post('/app/story_details/')
+      .send(inputData)
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    //assert response matches expected output
+    expect(response.body).toEqual({message: "Story Details Received Successfully", data: inputData});
+
+  });
+});
+
+//Test Case 4: Story Details Unsuccessfully Receives Story Details
+describe('POST /app/story_details/', () => {
+
+  it('should return 404 and an error message with the input data', async () => {
+    
+    //Define input data
+    const inputData = {"data": null};
+    
+    //Post Request
+    const response = await request(app)
+      .post('/app/story_details/')
+      .send(inputData)
+      .expect('Content-Type', /json/)
+      .expect(404);
+
+    //assert response matches expected output
+    expect(response.body).toEqual({message: "Story Details were not Received", data: inputData});
+
+  });
+});
+
+//Test Case 5: Extra Details Successfully Receives Story Details
+describe('POST /app/extra_details/', () => {
+
+  it('should return 200 and a success message with the input data', async () => {
+    
+    //Define input data
+    const inputData = {"data": "Story Detail"};
+    
+    //Post Request
+    const response = await request(app)
+      .post('/app/extra_details/')
+      .send(inputData)
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    //assert response matches expected output
+    expect(response.body).toEqual({message: "Extra Details Received Successfully", data: inputData});
+
+  });
+});
+
+//Test Case 6: Extra Details Unsuccessfully Receives Story Details
+describe('POST /app/extra_details/', () => {
+
+  it('should return 404 and an error message with the input data', async () => {
+    
+    //Define input data
+    const inputData = {"data": null};
+    
+    //Post Request
+    const response = await request(app)
+      .post('/app/extra_details/')
+      .send(inputData)
+      .expect('Content-Type', /json/)
+      .expect(404);
+
+    //assert response matches expected output
+    expect(response.body).toEqual({message: "Extra Details were not Received", data: inputData});
+
+  });
+});
+
+//Test Case 7: Extra Details Successfully Receives Story Details
+describe('POST /app/courier_response/', () => {
+
+  it('should return 200 and a success message with the input data', async () => {
+    
+    //Define input data
+    const inputData = {"data": "Story Detail"};
+    
+    //Post Request
+    const response = await request(app)
+      .post('/app/courier_response/')
+      .send(inputData)
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    //assert response matches expected output
+    expect(response.body).toEqual({message: "Courier Response Received Successfully", data: inputData});
+
+  });
+});
+
+//Test Case 8: Extra Details Unsuccessfully Receives Story Details
+describe('POST /app/courier_response/', () => {
+
+  it('should return 404 and an error message with the input data', async () => {
+    
+    //Define input data
+    const inputData = {"data": null};
+    
+    //Post Request
+    const response = await request(app)
+      .post('/app/courier_response/')
+      .send(inputData)
+      .expect('Content-Type', /json/)
+      .expect(404);
+
+    //assert response matches expected output
+    expect(response.body).toEqual({message: "Courier Response were not Received", data: inputData});
 
   });
 });
--- storybuilder/Backend/routes/translator.js
+++ storybuilder/Backend/routes/translator.js
@@ -18,22 +18,20 @@ router.post('/app/chapter_count/', (req, res) => {
         res.status(400).json({message: "Invalid Data Type", data: req.body});
     }
 
-    console.log("POST Input: " + chapter_count);
-
     //Send Successful Response Back to Frontend
     res.status(200).json({message: "Chapter Count Received Successfully", data: req.body});
 });
 
 //story_details will receive the details of the story
 router.post('/app/story_details/', (req, res) => {
-    //Storing Body Data
-    story_details = JSON.stringify(req.body);
-    console.log("POST Input: " + story_details);
 
     //If data was not received successfully
-    if (story_details == null){
-        res.status(404).json({message: "Chapter Count was not Received", data: req.body});
+    if (req.body.data == null){
+        res.status(404).json({message: "Story Details were not Received", data: req.body});
     }
+
+    //Storing Body Data
+    story_details = JSON.stringify(req.body.data);
 
     //Send Successful Response Back to Frontend
     res.status(200).json({message: "Story Details Received Successfully", data: req.body});
@@ -41,14 +39,14 @@ router.post('/app/story_details/', (req, res) => {
 
 //extra_details will receive any additional requests to take into account when drafting the story
 router.post('/app/extra_details/', (req, res) => {
-    //Storing Body Data
-    extra_details = JSON.stringify(req.body);
-    console.log("POST Input: " + extra_details);
 
     //If data was not received successfully
-    if (extra_details == null){
-        res.status(404).json({message: "Chapter Count was not Received", data: req.body});
+    if (req.body.data == null){
+        res.status(404).json({message: "Extra Details were not Received", data: req.body});
     }
+
+    //Storing Body Data
+    extra_details = JSON.stringify(req.body.data);
 
     //Send Successful Response Back to Frontend
     res.status(200).json({message: "Extra Details Received Successfully", data: req.body});
@@ -56,17 +54,17 @@ router.post('/app/extra_details/', (req, res) => {
 
 //courier_response will receive any additional requests to take into account when drafting the story
 router.post('/app/courier_response/', (req, res) => {
-    //Storing Body Data
-    courier_response = JSON.stringify(req.body);
-    console.log("POST Input: " + courier_response);
 
     //If data was not received successfully
-    if (extra_details == null){
-        res.status(404).json({message: "Chapter Count was not Received", data: req.body});
+    if (req.body.data == null){
+        res.status(404).json({message: "Courier Response were not Received", data: req.body});
     }
 
+    //Storing Body Data
+    courier_response = JSON.stringify(req.body.data);
+
     //Send Successful Response Back to Frontend
-    res.status(200).json({message: "Extra Details Received Successfully", data: req.body});
+    res.status(200).json({message: "Courier Response Received Successfully", data: req.body});
 });
 
 // Export the routers for use in app.js
