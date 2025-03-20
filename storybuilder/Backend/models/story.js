f4c648fb4bf3a1f83f254ba9cb2766dac3016831
qari7 <tuo65470@temple.edu>
2025-03-20 01:12:10 -0400

    added agent and story data model

--- storybuilder/Backend/models/story.js
+++ storybuilder/Backend/models/story.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 
 const storySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  prompt_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Prompt', required: false },
-  chapters: [{
-    title: { type: String },
-    content: { type: String },
-    agents: [{
-      agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
-      chapterVersion: { type: String }, // Store agent-specific version of the chapter
-      votes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Vote' }],
-      critiques: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Critique' }]
+    title: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    prompt_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Prompt', required: false },
+    chapters: [{
+        chapter_number: { type: String },
+        content: { type: String },
+        agent_chapter_versions: [{
+            agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
+            chapter_version: { type: String }, // Store agent-specific version of the chapter
+            votes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Vote' }],
+            critiques: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Critique' }]
+        }]
     }]
-  }],
-  createdAt: { type: Date, default: Date.now }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Story', storySchema);
