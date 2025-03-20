9642f56248a01120f247ba9f3f77ddd0d502a8ef
qari7 <tuo65470@temple.edu>
2025-03-20 00:37:30 -0400

    added a data model for users.js, untested. also hashes the password

---
+++ storybuilder/Backend/models/users.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true } 
+}, { timestamps: true });
+
+// Hash password before saving to DB
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();  // Only hash if password is modified
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Compare input password with stored hash
+UserSchema.methods.comparePassword = async function (inputPassword) {
+    return await bcrypt.compare(inputPassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
