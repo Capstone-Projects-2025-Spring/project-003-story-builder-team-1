d4d782ed01a6767f8d98c5b7aa92aaf9df21e92f
Christopher McGarrity <119720325+CBerg333@users.noreply.github.com>
2025-03-24 00:54:58 +0000

    Changed req body variable back to .data

--- storybuilder/Backend/routes/prompt_admin.js
+++ storybuilder/Backend/routes/prompt_admin.js
@@ -5,11 +5,11 @@ const promptformatter = require('../promptformatter')
 
 router.post('/story', async (req, res) => {
     //throws 400 status 
-    if(!req.body.story_details) {
+    if(!req.body.data) {
        return res.status(400).json({message: "No prompt data received", data: req.body});
     }
     //formatting the story & extra details (ignoring story_name and chapter_count for now, also ignores the need for a previous_chapter entry)
-    const promptinfo = JSON.stringify(req.body.story_details);
+    const promptinfo = JSON.stringify(req.body.data);
     console.log("story_details taken in as promptinfo: "+ promptinfo);
 
     //sends to promptformatter to be organized in an acceptable format for the llama API
@@ -27,23 +27,18 @@ router.post('/story', async (req, res) => {
 });
 
 
-//prompt
-router.get('/prompt', (req, res) => {
+//next chapter
+router.get('/nextchapter', (req, res) => {
     res.status(200).json({message: "Data Received Successfully", data: req.body});
 });
 
 //refine_prompt
-router.get('/app/prompt_admin', (req, res) => {
-    res.status(200).json({message: "Data Received Successfully", data: req.body});
-});
-
-//generate_prompt
-router.get('/app/generate_prompt', (req, res) => {
+router.get('/critique', (req, res) => {
     res.status(200).json({message: "Data Received Successfully", data: req.body});
 });
 
 //rank_prompt
-router.get('/app/rank_prompt', (req, res) => {
+router.get('/judge', (req, res) => {
     res.status(200).json({message: "Data Received Successfully", data: req.body});
 });
 
