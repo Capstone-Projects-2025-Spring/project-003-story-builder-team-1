32d50eb30e248d5715baa5398bf36ce333dfa5ce
qari7 <tuo65470@temple.edu>
2025-03-26 16:15:24 -0400

    revert name change

--- storybuilder/Backend/models/story.js
+++ storybuilder/Backend/models/story.js
@@ -5,7 +5,7 @@ const storySchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     prompt_id: { type: String, required: true },
     chapters: [{
-        chapter_count: { type: String },
+        chapter_number: { type: String },
         content: { type: String },
         agent_chapter_versions: [{
             agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
