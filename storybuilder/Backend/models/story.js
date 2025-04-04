83c62a334633e424c62d21e99e51a350227464a4
qari7 <tuo65470@temple.edu>
2025-04-04 16:34:00 -0400

    updated story data model schema

--- storybuilder/Backend/models/story.js
+++ storybuilder/Backend/models/story.js
@@ -4,16 +4,19 @@ const storySchema = new mongoose.Schema({
     story_name: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     prompt: { type: String, required: true },
-    chapters: [{
-        chapter_number: { type: Number },
-        content: { type: String },
-        agent_chapter_versions: [{
-            agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
-            agent_chapter_version_content: { type: String }, // Store agent-specific version of the chapter
-            votes: [{ type: Number }],
-            critiques: { type: String }
+    story_content: [{
+        chapter_number: { type: Number, required: true },
+        text: { type: String, required: true }
+    }],
+    agents: [{
+        agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
+        chapters: [{
+            chapter_number: { type: Number },
+            content: { type: String },
+            votes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Vote' }],
+            critiques: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Critique' }]
         }]
-    }]
+    }],
 }, { timestamps: true });
 
 // Virtual property to generate the URL for a story
