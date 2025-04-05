67a1717287e62cceee1bed2f75da8fb585698773
qari7 <tuo65470@temple.edu>
2025-04-04 22:41:34 -0400

    added and removed routes

--- storybuilder/Backend/routes/db.js
+++ storybuilder/Backend/routes/db.js
@@ -25,11 +25,11 @@ router.get("/user/:id", user_controller.user_detail);
 
 /// STORY ROUTES ///
 
-// todo:
-// get outline, critique and chapter
-// update for all^
-// updating conent of story (vetoing)
-// editing and critiquing (CJ)
+// GET request for list of all Stories
+router.get("/stories", story_controller.story_list);
+
+// GET request for one Story
+router.get("/story/:id", story_controller.story_detail);
 
 // POST request for creating Story
 router.post("/story/create", story_controller.story_create_post);
@@ -40,27 +40,40 @@ router.post("/story/:id/delete", story_controller.story_delete_post);
 // POST request to update Story
 router.post("/story/:id/update", story_controller.story_update_post);
 
-// GET request for one Story
-router.get("/story/:id", story_controller.story_detail);
+// STORY ROUTES - CHAPTER (VETO, VOTING)
 
-// GET request for list of all Stories
-router.get("/stories", story_controller.story_list);
+// GET request for a chapter specific to a story 
+router.get("/story/:id/:chapter_number", story_controller.story_chapter_detail)
+
+// POST request to edit a chapter specific to a story (update chapter content)
+router.post("/story/:id/:chapter_number/edit", story_controller.story_chapter_edit_post);
+
+// POST request to choose an agent's version of a chapter to be the main chapter content
+router.post("/story/:id/:chapter_number/veto/:agent_id", story_controller.story_chapter_veto_post);
+
+// STORY ROUTES - OUTLINE
+
+// POST request for creating outline for a specific story
+router.post("/story/:id/outline/create", story_controller.story_outline_create_post);
+
+// GET request to retrieve the outline specific to a story
+router.get("/story/:id/outline", story_controller.story_outline);
 
 /// AGENT ROUTES ///
+
+// GET request for list of all Agents
+router.get("/agents", agent_controller.agent_list);
+
+// GET request for one Agent
+router.get("/agent/:id", agent_controller.agent_detail);
 
 // POST request for creating new instance of an Agent
 router.post("/agent/create", agent_controller.agent_create_post);
 
 // POST request for deleting an instance of an Agent
-router.post("agent/:id/delete", agent_controller.agent_delete_post);
+router.post("/agent/:id/delete", agent_controller.agent_delete_post);
 
 // POST request for updating the agent_prompt of an Agent
-router.post("agent/:id/update", agent_controller.agent_update_post);
-
-// GET request for one Agent
-router.get("/agent/:id", agent_controller.agent_detail);
-
-// GET request for list of all Agents
-router.get("/agents", agent_controller.agent_list);
+router.post("/agent/:id/update", agent_controller.agent_update_post);
 
 module.exports = router;
