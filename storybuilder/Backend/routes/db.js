e62139efc98afff77bed08cabeea5ad228dda088
qari7 <tuo65470@temple.edu>
2025-04-08 14:14:57 -0400

    router for critique

--- storybuilder/Backend/routes/db.js
+++ storybuilder/Backend/routes/db.js
@@ -49,7 +49,8 @@ router.get("/story/:user_id/:story_id/get_number_of_chapters", story_controller.
 // POST request to add chapter to agent-specfic version
 router.post("/story/:user_id/:story_id/add_agent_chapter", story_controller.story_add_agent_chapter_post);
 
-// POST request to add a critique to a chapter
+// POST request to add a critique to an existing chapter
+router.post("/story/:user_id/:story_id/:agent_id/:chapter_number/add_critique", story_controller.story_add_critique_post);
 
 // POST request for vetoing
 
