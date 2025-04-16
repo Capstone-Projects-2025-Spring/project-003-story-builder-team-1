403960910e4f69628f2e2e447bcbf5a00850cdd0
JohnDaang <xJohnDangx@gmail.com>
2025-04-15 20:47:22 -0400

    protected route works for /home, brings back to login

--- storybuilder/Frontend/src/components/PROTECTED_ROUTE.js
+++ storybuilder/Frontend/src/components/PROTECTED_ROUTE.js
@@ -1,11 +1,11 @@
 import { Navigate } from "react-router-dom";
-import { USE_AUTH } from "./AuthContext";
+import { USE_AUTH } from "../context/AUTH_CONTEXT";
 
 const PROTECTED_ROUTE = ({ children }) => {
   const { is_authenticated } = USE_AUTH();
 
   if (!is_authenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
